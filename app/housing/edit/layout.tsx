525930d2e0a3540129f288792d44042f8c94058d
alititi-jackie <fengjiancheng8@gmail.com>
2026-05-29 22:57:14 -0400

    chore: add noindex robots protections (#292)

---
+++ app/housing/edit/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from 'next'
+
+const NOINDEX_METADATA: Metadata = {
+  robots: {
+    index: false,
+    follow: false,
+    nocache: true,
+    googleBot: {
+      index: false,
+      follow: false,
+      noimageindex: true,
+      'max-video-preview': 0,
+      'max-image-preview': 'none',
+      'max-snippet': 0,
+    },
+  },
+}
+
+export const metadata = NOINDEX_METADATA
+
+export default function HousingEditLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return children
+}
