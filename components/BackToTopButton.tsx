645dd9b9f9b0e4fe353212b96a918b4ab160190e
alititi-jackie <fengjiancheng8@gmail.com>
2026-05-30 00:18:24 -0400

    style: align back to top button with app shell (#298)

--- components/BackToTopButton.tsx
+++ components/BackToTopButton.tsx
@@ -29,7 +29,7 @@ export default function BackToTopButton() {
       type="button"
       onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
       aria-label="回到顶部"
-      className="fixed right-4 z-50 flex h-11 w-11 items-center justify-center rounded-full border border-blue-100 bg-white shadow-md text-blue-600"
+      className="fixed right-4 md:right-[calc((100vw-760px)/2+16px)] lg:right-[calc((100vw-960px)/2+16px)] xl:right-[calc((100vw-1040px)/2+16px)] z-50 flex h-11 w-11 items-center justify-center rounded-full border border-blue-100 bg-white shadow-md text-blue-600"
       style={{ bottom: '88px' }}
     >
       <ChevronUp size={22} />
